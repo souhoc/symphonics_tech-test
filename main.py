4845a8d3070ad9bec7eee842af29dd6504fd96f5
sou.hoc <souhoc@icloud.com>
2025-07-23 11:42:26 +0200

    add report route

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import uvicorn
 
 from fastapi import FastAPI
 
-from .routes import devices
+from .routes import devices, report
 
 app = FastAPI()
 
 # Include routes
 app.include_router(devices.router, tags=["devices"])
+app.include_router(report.router, tags=["devices"])
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, log_level="info")
