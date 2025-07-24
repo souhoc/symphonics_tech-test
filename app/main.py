4ef526b3ae7363f2a6b6a12a7f20faf8d81b12c9
sou.hoc <souhoc@icloud.com>
2025-07-24 14:33:41 +0200

    set up basic logging

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 import uvicorn
+import logging
 from fastapi import FastAPI
-from app.endpoints import device, report
+
+from app.endpoints import device
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(name)s %(levelname)s %(message)s",
+    handlers=[logging.FileHandler("api.log"), logging.StreamHandler()],
+)
 
 app = FastAPI()
 
-app.include_router(device.rooter)
-app.include_router(report.router)
+app.include_router(device.router)
+# app.include_router(report.router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, log_level="info")
-
