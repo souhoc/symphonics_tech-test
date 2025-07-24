5e83b89f8937eda06c8560e8102e794c23c2ae70
sou.hoc <souhoc@icloud.com>
2025-07-24 14:33:07 +0200

    add(models): device_update
    As it is the only thing stored in DB. It's the only model

---
+++ app/models/device_update.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, validator 
+from datetime import datetime
+
+class DeviceUpdate(BaseModel):
+    device_id: str # XXX: might want to make it uuid
+    product_id: str
+    code: str
+    value: int
+    time:  datetime
+
+    @validator("time")
+    def check_time(cls, v: datetime):
+        if v.minute != 0 or v.second != 0 or v.microsecond != 0:
+            raise ValueError("Time must be on the hour.")
+        return v
